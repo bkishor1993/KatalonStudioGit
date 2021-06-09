c2bd1950db34533e242230687cf8e4138be405c6
Braj <Braj@DESKTOP-7F86NCR>
2021-06-10 03:08:50 +0800

    TC_01 committed

---
+++ Test Suites/TC_01.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TC_01</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>e194c9cb-35c2-48d0-8c1e-91bd8667f458</testSuiteGuid>
+   <testCaseLink>
+      <guid>d81b9d45-9c7f-425e-9901-78fddf802906</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test1/Login</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
